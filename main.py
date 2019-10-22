90e8cb0840a5c58e373fb90138e7b43858e741f4
João Paulo Back <joaoback47@gmail.com>
2019-10-22 11:28:13 -0300

    #3

--- main.py
+++ main.py
@@ -4,54 +4,59 @@ import random as random
 import math
 
 class Perceptron:
-  def __init__(self, dataset, learningRate = 0.5, epochs = 1000):
-    self.dataset = dataset
+  def __init__(self, x1, x2, x3, outputs, learningRate = 1, epochs = 1000):
+
+    self.x1 = x1
+    self.x2 = x2
+    self.x3 = x3
+    self.outputs = outputs
     self.weights = []
-    self.outputs = []
+    self.bias = 0
+    self.thresold = 10
     self.learningRate = learningRate
     self.epochs = epochs
 
   def initWeights(self):
-    for i in range(len(self.dataset)):
+    self.bias = random.random()
+    for i in range(len(self.x1)):
       self.weights.append(random.random())
-
-  def sigmoid(self, x):
-    return (1 / (1 + math.exp(-x)))
   
-  def recalcWeights(self, difference, inputs):
-    for i in range(len(inputs)):
-      self.weights[i] = self.weights[i] + self.learningRate * difference * inputs[i]
-  def run(self, dataset):
-    sum = 0
-    for i in range(len(dataset)):
-      sum = sum + (dataset)
+  def recalcWeights(self, error, x, y, z):
+    self.weights[0] = self.weights[0] + self.learningRate * error * x
+    self.weights[1] = self.weights[1] + self.learningRate * error * y
+    self.weights[2] = self.weights[2] + self.learningRate * error * z
+    self.bias = self.bias + self.learningRate * error
+
+  def calculateOutput(self, x, y, z):
+    sum = x * self.weights[0] + y * self.weights[1] +z * self.weights[2] + self.bias
+    if(sum >= self.thresold):
+      return 1
+    else:
+      return -1
+
   def train(self):
     self.initWeights()
-    
-    error = False
-    i = 0
-
-    while(error and i < self.epochs):
-      difference = 0
-
-      for i in range(len(self.dataset)):
-        result = self.run(self.dataset[['x1', 'x2', 'x3']])
-        if result != self.outputs[i]:
-          error = True
-          difference = self.outputs[i] - result
-          self.recalcWeights(difference, p.dataset[['x1', 'x2', 'x3']])
-        else:
-          error = False
-      print("Interaction => " + i)
-      i = i + 1
+    e = 0
+    while(True):
+      e = e + 1
+      globalError = 0
+      for i in range(len(self.x1)):
+        result = self.calculateOutput(self.x1[i], self.x2[i], self.x2[i])
+        print("Expected is {expected} and Predicted is {predicted}".format(expected=self.outputs[i], predicted=result))
+        localError = self.outputs[i] - result
+        self.recalcWeights(localError, self.x1[i], self.x2[i], self.x2[i])
+        globalError += (localError * localError)
+      print("Iteration => {e} with error {error}".format(e = e, error = math.sqrt(globalError/len(self.x1))))
+      if(globalError == 0 or e >= self.epochs):
+        break
+    print("Decision linear equation found => {output1}*x1 + {output2}*x2 + {output3}*x3 + {bias} = 0".format(output1=self.outputs[0], output2=self.outputs[1], output3=self.outputs[2], bias=self.bias))
+  #endfunction
 
 def openFile(path):
-    with open(path) as dataset:
-      df = pd.read_excel(path)
-      return df
+  with open(path) as dataset:
+    df = pd.read_excel(path)
+    return df
 
-p = Perceptron(openFile('Dados_Treinamento_Perceptron.xls'))
-print(p.dataset[['x1', 'x2', 'x3']][0])
-
-
-
+dataset = openFile('Dados_Treinamento_Perceptron.xls')
+p = Perceptron(dataset['x1'], dataset['x2'], dataset['x3'], dataset['d'])
+p.train()
