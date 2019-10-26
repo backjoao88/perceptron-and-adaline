a4a5a8603a31024a4aff35683860c5b9321a00b2
Joao Paulo Back <joaoback47@gmail.com>
2019-10-26 17:18:54 -0300

    #5

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import math
 
 class Perceptron:
 
-  def __init__(self, x1, x2, x3, outputs, learningRate = 0.1, epochs = 10000):
+  def __init__(self, x1, x2, x3, outputs, learningRate = 0.1   , epochs = 2000):
+    
     self.x1 = x1
     self.x2 = x2
     self.x3 = x3
+
     self.outputs = outputs
     self.weights = []
     self.bias = 0
@@ -17,9 +19,9 @@ class Perceptron:
     self.epochs = epochs
 
   def initWeights(self):
-    self.bias = random.random()
+    self.bias = 0
     for i in range(len(self.x1)):
-      self.weights.append(random.random())
+      self.weights.append(0)
   #endfunction
   
   def recalcWeights(self, error, x, y, z):
@@ -52,7 +54,7 @@ class Perceptron:
       print("Iteration => {e} with error {error}".format(e = e, error = math.sqrt(globalError/len(self.x1))))
       if(globalError == 0 or e >= self.epochs):
         break
-    print("Decision linear equation found => {weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], bias=self.bias))
+    print("Decision boundary line => {weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], bias=self.bias))
   #endfunction
 
 def openFile(path):
