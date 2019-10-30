09049838850c8562426aa7ddc47603ab865a89d4
João Paulo Back <joaoback47@gmail.com>
2019-10-30 11:39:44 -0300

    %5

--- adaline.py
+++ adaline.py
@@ -5,7 +5,7 @@ import math
 
 class Adaline:
 
-  def __init__(self, x1, x2, x3, outputs, learningRate = 0.1, epochs = 2000):
+  def __init__(self, x1, x2, x3, outputs, learningRate = 0.0001, requiredPrecision = 0.1, epochs = 2000):
     
     self.x1 = x1
     self.x2 = x2
@@ -14,9 +14,19 @@ class Adaline:
     self.outputs = outputs
     self.weights = []
     self.bias = 0
-    self.thresold = 0
     self.learningRate = learningRate
+    self.requiredPrecision = requiredPrecision
     self.epochs = epochs
+
+  def EQM(self):
+    sizeTrainingSample = len(self.x1)
+    EQM = 0
+    for i in range(len(sizeTrainingSample)):
+      u = self.calculateOutput(self.x1[i], self.x2[i], self.x3[i])
+      EQM += EQM + ((self.outputs[i] - u) ** 2)
+    EQM = EQM / sizeTrainingSample
+    return EQM
+  #endfunction
 
   def initWeights(self):
     self.bias = 0
@@ -26,7 +36,7 @@ class Adaline:
   
   def recalcWeights(self, error, x, y, z):
     # EQM
-    self.weights[0] = self.weights[0] * error * x
+    self.weights[0] = self.weights[0] * learningRate
     self.weights[1] = self.weights[1] + self.learningRate * error * y
     self.weights[2] = self.weights[2] + self.learningRate * error * z
     self.bias = self.bias + self.learningRate * error
@@ -40,33 +50,20 @@ class Adaline:
       return -1
   #endfunction
 
-  def validate(self, x1, x2, x3):
-    line = self.train()
-    for i in range(len(x1)):
-      sum = x1[i] * line[0] + x2[i] * line[1] + x3[i] * line[2] + line[3]
-      if(sum >= 0):
-        result = 1
-      else:
-        result = 0
-      print("Iteration = {i}".format(i=i))
-      print("x1={x1}, x2={x2}, x3={x3}, b={bias} = {result}".format(x1=x1[i], x2=x2[i], x3=x3[i], bias=line[3], result=result))
-  #endfunction
-
   def train(self):
     self.initWeights()
     e = 0
+
+    previousEQM = self.EQM()
     while(True):
-      e = e + 1
-      globalError = 0
       for i in range(len(self.x1)):
         result = self.calculateOutput(self.x1[i], self.x2[i], self.x3[i])
-        localError = (self.outputs[i] - result) ** 2
         self.recalcWeights(localError, self.x1[i], self.x2[i], self.x3[i])
-        globalError += (localError * localError)
-      print("Epoch => {e} with error {error}".format(e = e, error = math.sqrt(globalError/len(self.x1))))
-      if(globalError == 0 or e >= self.epochs):
+      e = e + 1
+      currentEQM = self.EQM()
+      print("Current EQM -> {eqm}".format(eqm = currentEQM))
+      if (currentEQM - previousEQM) < self.requiredPrecision:
         break
-    print("Decision boundary line => {weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], bias=self.bias))
     return [self.weights[0], self.weights[1], self.weights[2], self.bias]
   #endfunction
 
@@ -77,10 +74,9 @@ def openFile(path):
 #endfunction
 
 dataset = openFile('Dados_Treinamento_Perceptron.xls')
-datasetV = openFile('Dados_Validação_Perceptron.xls')
 
-p = Perceptron(dataset['x1'], dataset['x2'], dataset['x3'], dataset['d'])
-p.validate(datasetV['x1'], datasetV['x2'], datasetV['x3'])
+p = Adaline(dataset['x1'], dataset['x2'], dataset['x3'], dataset['d'])
+p.train()
 
 
 
