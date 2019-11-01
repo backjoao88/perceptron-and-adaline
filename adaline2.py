8eea0959629af45a4b4bc4b397a740a70e888969
Joao Paulo Back <joaoback47@gmail.com>
2019-11-01 11:37:43 -0300

    %7

--- adaline2.py
+++ adaline2.py
@@ -6,11 +6,12 @@ import math
 
 class Adaline:
 
-  def __init__(self, x1, x2, x3, outputs, learningRate = 0.01, requiredPrecision = 0.00001):
+  def __init__(self, x1, x2, x3, x4, outputs, learningRate = 0.01, requiredPrecision = 0.00001):
     
     self.x1 = x1
     self.x2 = x2
     self.x3 = x3
+    self.x4 = x4
     self.outputs = outputs
     
     self.weights = []
@@ -23,7 +24,7 @@ class Adaline:
     EQM = 0
     #print(self.x1)
     for i in range(len(self.x1)):
-      u = self.calculateOutput(self.x1[i], self.x2[i], self.x3[i])
+      u = self.calculateOutput(self.x1[i], self.x2[i], self.x3[i], self.x4[i])
       EQM += (self.outputs[i] - u) ** 2
     EQM = EQM / sizeTrainingSample
     return EQM
@@ -35,8 +36,8 @@ class Adaline:
       self.weights.append(0)
   #endfunction
   
-  def calculateOutput(self, x, y, z):
-    output = x * self.weights[0] + y * self.weights[1] + z * self.weights[2] + self.bias
+  def calculateOutput(self, x, y, z, p):
+    output = x * self.weights[0] + y * self.weights[1] + z * self.weights[2] + p * self.weights[3] + self.bias
     return output
   #endfunction
 
@@ -61,17 +62,18 @@ class Adaline:
     while(True):
       previousEQM = self.EQM()
       for i in range(len(self.x1)):
-        u = self.calculateOutput(self.x1[i], self.x2[i], self.x3[i])
+        u = self.calculateOutput(self.x1[i], self.x2[i], self.x3[i], self.x4[i])
         self.weights[0] += self.learningRate * (self.outputs[i] - u) * self.x1[i]
         self.weights[1] += self.learningRate * (self.outputs[i] - u) * self.x2[i]
         self.weights[2] += self.learningRate * (self.outputs[i] - u) * self.x3[i]
+        self.weights[3] += self.learningRate * (self.outputs[i] - u) * self.x4[i]
         self.bias       += self.learningRate * (self.outputs[i] - u) 
       e = e + 1
       currentEQM = self.EQM()
-      print("{weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], bias=self.bias))
+      print("{weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {weight4}*x4 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], weight4=self.weights[3], bias=self.bias))
       if abs(currentEQM - previousEQM) <= self.requiredPrecision:
         break
-    return [self.weights[0], self.weights[1], self.weights[2], self.bias]
+    return [self.weights[0], self.weights[1], self.weights[2], self.weights[3], self.bias]
   #endfunction
 
 def openFile(path):
@@ -80,19 +82,9 @@ def openFile(path):
     return df
 #endfunction
 
-dataset = openFile('Dados_Treinamento_Perceptron.xls')
-datasetV = openFile('Dados_Validação_Perceptron.xls')
+dataset = openFile('Dados_Treinamento_Adaline.xls')
+print(dataset['x1'])
+p = Adaline(dataset['x1'], dataset['x2'], dataset['x3'], dataset['x4'], dataset['d'])
+#p.validate(dataset['x1'], dataset['x2'], dataset['x3'])
+p.train()
 
-p = Adaline(dataset['x1'], dataset['x2'], dataset['x3'], dataset['d'])
-p.validate(dataset['x1'], dataset['x2'], dataset['x3'])
-#p.train()
-
-
-
-# => Treinamento: 
-# Pesos Sinápticos - Weights (Obs: Posição 0 é o bias):
-# Iniciais => [0. 0. 0. 0.]
-# Finais   => [2.6260084416312894, 1.5496181751568099, 2.3748149335864213, -0.6750827626238209]
-# Número de Épocas => 405
-# Taxa de Aprendizado => 0.01
-# Precisão requerida => 1e-05
--- perceptron.py
+++ perceptron.py
@@ -5,16 +5,13 @@ import math
 
 class Perceptron:
 
-  def __init__(self, x1, x2, x3, outputs, learningRate = 0.1   , epochs = 2000):
-    
+  def __init__(self, x1, x2, x3, outputs, learningRate = 0.1   , epochs = 500):
     self.x1 = x1
     self.x2 = x2
     self.x3 = x3
-
     self.outputs = outputs
     self.weights = []
     self.bias = 0
-    self.thresold = 0
     self.learningRate = learningRate
     self.epochs = epochs
 
@@ -33,13 +30,13 @@ class Perceptron:
   
   def calculateOutput(self, x, y, z):
     sum = x * self.weights[0] + y * self.weights[1] +z * self.weights[2] + self.bias
-    if(sum >= self.thresold):
+    if(sum >= 0):
       return 1
     else:
       return -1
   #endfunction
 
-  def validate(self, x1, x2, x3):
+  def validate(self, x1, x2, x3, d):
     line = self.train()
     for i in range(len(x1)):
       sum = x1[i] * line[0] + x2[i] * line[1] + x3[i] * line[2] + line[3]
@@ -47,25 +44,28 @@ class Perceptron:
         result = 1
       else:
         result = -1
-      print("Iteration = {i}".format(i=i))
-      print("x1={x1}, x2={x2}, x3={x3}, b={bias} = {result}".format(x1=x1[i], x2=x2[i], x3=x3[i], bias=line[3], result=result))
+    print("Validation Data -> ")
+    print("Predicted Data -> ")
+    print("Error Percentage -> ")
+  #endfunction
 
   def train(self):
     self.initWeights()
+    print("Initial Weights => {weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], bias=self.bias))
     e = 0
+    print("Learning Rate -> {lr}".format(lr=self.learningRate))
     while(True):
       e = e + 1
       globalError = 0
       for i in range(len(self.x1)):
         result = self.calculateOutput(self.x1[i], self.x2[i], self.x3[i])
-        #print("Expected is {expected} and Predicted is {predicted}".format(expected=self.outputs[i], predicted=result))
         localError = self.outputs[i] - result
         self.recalcWeights(localError, self.x1[i], self.x2[i], self.x3[i])
         globalError += (localError * localError)
-      print("Epoch => {e} with error {error}".format(e = e, error = math.sqrt(globalError/len(self.x1))))
       if(globalError == 0 or e >= self.epochs):
         break
-    print("Decision boundary line => {weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], bias=self.bias))
+    print("Final Weights => {weight1}*x1 + {weight2}*x2 + {weight3}*x3 + {bias} = 0".format(weight1=self.weights[0], weight2=self.weights[1], weight3=self.weights[2], bias=self.bias))
+    print("Epoch {e} - error {error}".format(e =e, error=math.sqrt(globalError/len(self.x1))))
     return [self.weights[0], self.weights[1], self.weights[2], self.bias]
   #endfunction
 
@@ -73,12 +73,10 @@ def openFile(path):
   with open(path) as dataset:
     df = pd.read_excel(path)
     return df
+#endfunction
 
 dataset = openFile('Dados_Treinamento_Perceptron.xls')
 datasetV = openFile('Dados_Validação_Perceptron.xls')
 
 p = Perceptron(dataset['x1'], dataset['x2'], dataset['x3'], dataset['d'])
-p.validate(datasetV['x1'], datasetV['x2'], datasetV['x3'])
-
-
-
+p.train()
